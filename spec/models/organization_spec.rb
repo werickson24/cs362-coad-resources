f78480b67ef772423834766bf2e80e642a507960
Devon Ceccacci <dceccacci@gmail.com>
2024-01-22 13:31:16 -0800

    added "responde_to" tests to organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-  it "exists" do
-    Organization.new
+  it "has a email" do
+    organization = Organization.new
+    expect(organization).to respond_to(:email)
   end
+
+  it "has a name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:name)
+  end
+
+  it "has a phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:phone)
+  end
+
+  it "has status" do
+    organization = Organization.new
+    expect(organization).to respond_to(:status)
+  end
+
+  it "has a primary_name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:primary_name)
+  end
+
+  it "has a secondary_name" do
+    organization = Organization.new
+    expect(organization).to respond_to(:primary_name)
+  end
+
+  it "has a secondary_phone" do
+    organization = Organization.new
+    expect(organization).to respond_to(:primary_name)
+  end
+
 
 end
