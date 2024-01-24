d70a73e216b618e8885ef75ad819e12ed41d3f52
Devon Ceccacci <dceccacci@gmail.com>
2024-01-24 12:24:35 -0800

    added active test

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let(:resourceCategory) { ResourceCategory.new }
+
   it "exists" do
     ResourceCategory.new
   end
 
   it "has a name" do
-    resourceCategory = ResourceCategory.new
     expect(resourceCategory).to respond_to(:name)
+  end
+
+  it "has active" do
+    expect(resourceCategory).to respond_to(:active)
   end
 
 end
