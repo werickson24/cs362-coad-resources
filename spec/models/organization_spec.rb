2c355a707d853cea55fbd5558b8847c822221e67
Devon Ceccacci <dceccacci@gmail.com>
2024-01-24 12:16:59 -0800

    added liability_insurance, title, transportation existence tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,50 +2,54 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  let(:organization) { Organization.new }
+
   it "has a email" do
-    organization = Organization.new
     expect(organization).to respond_to(:email)
   end
 
   it "has a name" do
-    organization = Organization.new
     expect(organization).to respond_to(:name)
   end
 
   it "has a phone" do
-    organization = Organization.new
     expect(organization).to respond_to(:phone)
   end
 
   it "has status" do
-    organization = Organization.new
     expect(organization).to respond_to(:status)
   end
 
   it "has a primary_name" do
-    organization = Organization.new
     expect(organization).to respond_to(:primary_name)
   end
 
   it "has a secondary_name" do
-    organization = Organization.new
     expect(organization).to respond_to(:secondary_name)
   end
 
   it "has a secondary_phone" do
-    organization = Organization.new
     expect(organization).to respond_to(:secondary_phone)
   end
 
   it "has a description" do
-    organization = Organization.new
     expect(organization).to respond_to(:description)
   end
 
   it "has a rejection_reason" do
-    organization = Organization.new
     expect(organization).to respond_to(:rejection_reason)
   end
 
+  it "has liability_insurance"
+    expect(organization).to respond_to(:liability_insurance)
+  end
+
+  it "has a title"
+    expect(organization).to respond_to(:title)
+  end
+
+  it "has transportation"
+    expect(organization).to respond_to(:transportation)
+  end
 
 end
