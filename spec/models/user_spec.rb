08a5e12fbc9ff9a375a3a82d76feff0348f71eff
William Erickson <william@ericksonls.com>
2024-01-24 16:19:01 -0800

    added remaining validate tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,8 +22,40 @@ RSpec.describe User, type: :model do
       with_values([:admin, :organization])
   end
 
+  # after_initialize test
+    # unknown method
+
   # belongs_to tests
   it { should belong_to(:organization).optional}
 
+  # devise test
+    # unknown method
+
+  # validates tests
+
+  it { should validate_presence_of(:email)}
+
+  it do
+    should validate_length_of(:email).
+      is_at_least(1).
+      is_at_most(255).
+      on(:create)
+  end
+
+    # unknown test for validates
+
+  it { should validate_uniqueness_of(:email).case_insensitive }
+
+  it do
+    should validate_presence_of(:password).
+    on(:create)
+  end
+
+  it do
+    should validate_length_of(:password).
+      is_at_least(7).
+      is_at_most(255).
+      on(:create)
+  end
 
 end
