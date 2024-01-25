8ab3bd24452c5bae233ad6a51095573a963ce648
William Erickson <william@ericksonls.com>
2024-01-24 16:29:46 -0800

    finished adding tests to ticket

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -26,10 +26,37 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:resource_category_id)
   end
 
+  # belongs_to tests
+
   it { should belong_to(:region)}
 
   it { should belong_to(:resource_category)}
 
   it { should belong_to(:organization).optional}
+
+  # validates tests
+
+  it { should validate_presence_of(:name)}
+
+  it { should validate_presence_of(:phone)}
+
+  it { should validate_presence_of(:region_id)}
+
+  it { should validate_presence_of(:resource_category_id)}
+
+  it do
+    should validate_length_of(:name).
+      is_at_least(1).
+      is_at_most(255).
+      on(:create)
+  end
+
+  it do
+    should validate_length_of(:description).
+      is_at_most(1020).
+      on(:create)
+  end
+
+  # unknown test for scope
 
 end
