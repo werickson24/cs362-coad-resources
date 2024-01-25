307dd230eec117baf40bac0246e0f4f083c7e9f3
William Erickson <william@ericksonls.com>
2024-01-24 16:34:35 -0800

    added tests to resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+﻿require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
@@ -15,5 +15,26 @@ RSpec.describe ResourceCategory, type: :model do
   it "has active" do
     expect(resourceCategory).to respond_to(:active)
   end
+
+  # has tests
+
+  it { should have_and_belong_to_many(:organizations) }
+
+  it { should have_many(:tickets)}
+
+  # validates tests
+
+  it { should validate_presence_of(:name)}
+
+  it do
+    should validate_length_of(:name).
+      is_at_least(1).
+      is_at_most(255).
+      on(:create)
+  end
+
+  it { should validate_uniqueness_of(:name).case_insensitive }
+
+  # unknown test for scope
 
 end
