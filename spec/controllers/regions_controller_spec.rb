a88f7a1bb1c33e9d22074d4bd7fefd45414a7df0
Devon Ceccacci <dceccacci@gmail.com>
2024-02-23 14:44:59 -0800

    add tests for admin user

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -15,18 +15,73 @@ RSpec.describe RegionsController, type: :controller do
 		let(:user){create(:user, :admin)}
 		before(:each){sign_in(user)}
 
-    let(:region){create(:region)} # MAYBE
-		describe "POST #create" do
-		it {
-			post(:create, params: { region: attributes_for(:region) })	# attributes_for passes parameters for the region.
-			expect(response).to redirect_to(regions_path)
-		}
-    it {
-			expect_any_instance_of(Region).to receive(:save).and_return(false) # Now if save is called, it will return false
-			post(:create, params: { region: attributes_for(:region) })
-			expect(response).to be_successful
-    }
+    let(:region){create(:region)}
+
+    describe "GET #index" do
+      it {
+        get :index
+        expect(response).to be_successful
+      }
     end
+
+    describe "PATCH #show" do
+      it {
+        ticket = create(:ticket)
+        patch :show, params: {id: ticket.id}
+        expect(response).to be_successful
+      }
+    end
+
+    describe "GET #new" do
+      it{
+        get :new
+        expect(response).to be_successful
+      }
+    end
+
+		describe "POST #create" do
+      it {
+        post(:create, params: { region: attributes_for(:region) })	# attributes_for passes parameters for the region.
+        expect(response).to redirect_to(regions_path)
+      }
+      it {
+        expect_any_instance_of(Region).to receive(:save).and_return(false) # Now if save is called, it will return false
+        post(:create, params: { region: attributes_for(:region) })
+        expect(response).to be_successful
+      }
+    end
+
+    describe "GET #edit" do
+      it {
+        get(:edit, params: {id: region.id})
+        expect(response).to be_successful
+      }
+    end
+
+    describe "PATCH #update" do
+      it "updates" do
+        patch(:update, params: {id: region.id, region: attributes_for(:region)})  
+        expect(response).to redirect_to(region)
+      end
+      it "does not update" do
+        expect_any_instance_of(Region).to receive(:update).and_return(false)
+        patch(:update, params: {id: region.id, region: attributes_for(:region)})  
+        expect(response).to be_successful
+      end
+    end
+
+    describe "DELETE #destroy" do
+      let(:regionDelete){create(:region)}
+      it "deletes a region" do
+        delete(:destroy, params: {id: regionDelete.id})
+
+        expect(response).to redirect_to(regions_path)
+        expect(flash[:notice]).to match(/Region #{regionDelete.name} was deleted/)
+        expect(Region.where(id: regionDelete.id)).to be_empty
+      end
+    end
+
+
   end
 
   describe "edit" do
