77c42b15e25ea94de23d1e44e144d125d2164919
Devon Ceccacci <dceccacci@gmail.com>
2024-02-23 15:22:12 -0800

    added region controller tests logged-out, and organization

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,15 +1,6 @@
 ﻿require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
-  describe "index" do
-
-  end
-  describe "show" do
-
-  end
-  describe "new" do
-
-  end
 
   context 'as an admin user' do
 		let(:user){create(:user, :admin)}
@@ -41,11 +32,12 @@ RSpec.describe RegionsController, type: :controller do
 
 		describe "POST #create" do
       it {
-        post(:create, params: { region: attributes_for(:region) })	# attributes_for passes parameters for the region.
+        post(:create, params: { region: attributes_for(:region) })
         expect(response).to redirect_to(regions_path)
+        expect(flash[:notice]).to match(/Region successfully created/)
       }
       it {
-        expect_any_instance_of(Region).to receive(:save).and_return(false) # Now if save is called, it will return false
+        expect_any_instance_of(Region).to receive(:save).and_return(false)
         post(:create, params: { region: attributes_for(:region) })
         expect(response).to be_successful
       }
@@ -59,11 +51,12 @@ RSpec.describe RegionsController, type: :controller do
     end
 
     describe "PATCH #update" do
-      it "updates" do
+      it "redirects to region" do
         patch(:update, params: {id: region.id, region: attributes_for(:region)})  
         expect(response).to redirect_to(region)
+        expect(flash[:notice]).to match(/Region successfully updated/)
       end
-      it "does not update" do
+      it "updates successful" do
         expect_any_instance_of(Region).to receive(:update).and_return(false)
         patch(:update, params: {id: region.id, region: attributes_for(:region)})  
         expect(response).to be_successful
@@ -72,7 +65,7 @@ RSpec.describe RegionsController, type: :controller do
 
     describe "DELETE #destroy" do
       let(:regionDelete){create(:region)}
-      it "deletes a region" do
+      it "deletes successful" do
         delete(:destroy, params: {id: regionDelete.id})
 
         expect(response).to redirect_to(regions_path)
@@ -80,17 +73,127 @@ RSpec.describe RegionsController, type: :controller do
         expect(Region.where(id: regionDelete.id)).to be_empty
       end
     end
-
-
   end
 
-  describe "edit" do
 
-  end
-  describe "update" do
 
-  end
-  describe "destroy" do
+  context 'as a logged-out' do
 
+    let(:region){create(:region)}
+
+    describe "GET #index" do
+      it {
+        get :index
+        expect(response).not_to be_successful
+      }
+    end
+    
+    describe "PATCH #show" do
+      it {
+        ticket = create(:ticket)
+        patch :show, params: {id: ticket.id}
+        expect(response).not_to be_successful
+      }
+    end
+
+    describe "GET #new" do
+      it {
+        get :new
+        expect(response).not_to be_successful
+      }
+    end
+
+		describe "POST #create" do
+      it {
+        post(:create, params: { region: attributes_for(:region) })
+        expect(response).not_to redirect_to(regions_path)
+      }
+    end
+
+    describe "GET #edit" do
+      it {
+        get(:edit, params: {id: region.id})
+        expect(response).not_to be_successful
+      }
+    end
+    
+    describe "PATCH #update" do
+      it{
+        patch(:update, params: {id: region.id, region: attributes_for(:region)})  
+        expect(response).not_to redirect_to(region)
+      }
+    end
+
+    describe "DELETE #destroy" do
+      let(:regionDelete){create(:region)}
+      it {
+        delete(:destroy, params: {id: regionDelete.id})
+
+        expect(response).not_to redirect_to(regions_path)
+      }
+    end
   end
+
+
+
+  context 'as a organization' do
+    let(:user){create(:user, :organization_approved)}
+		before(:each){sign_in(user)}
+
+    let(:region){create(:region)}
+
+    describe "GET #index" do
+      it {
+        get :index
+        expect(response).not_to be_successful
+      }
+    end
+    
+    describe "PATCH #show" do
+      it {
+        ticket = create(:ticket)
+        patch :show, params: {id: ticket.id}
+        expect(response).not_to be_successful
+      }
+    end
+
+    describe "GET #new" do
+      it {
+        get :new
+        expect(response).not_to be_successful
+      }
+    end
+
+		describe "POST #create" do
+      it {
+        post(:create, params: { region: attributes_for(:region) })
+        expect(response).not_to redirect_to(regions_path)
+      }
+    end
+
+    describe "GET #edit" do
+      it {
+        get(:edit, params: {id: region.id})
+        expect(response).not_to be_successful
+      }
+    end
+    
+    describe "PATCH #update" do
+      it{
+        patch(:update, params: {id: region.id, region: attributes_for(:region)})  
+        expect(response).not_to redirect_to(region)
+      }
+    end
+
+    describe "DELETE #destroy" do
+      let(:regionDelete){create(:region)}
+      it {
+        delete(:destroy, params: {id: regionDelete.id})
+
+        expect(response).not_to redirect_to(regions_path)
+      }
+    end
+  end
+
+
 end
