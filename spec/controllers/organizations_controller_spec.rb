a2fa21c297b2f9c77da244dfabf60f1cc4cf4502
Devon Ceccacci <dceccacci@gmail.com>
2024-02-23 20:02:32 -0800

    added tests for a organization user with no organization assigned

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,28 +1,129 @@
 ﻿require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-  describe "index" do
+  context 'as an organization user without an Organization' do
     
-  end
-  describe "new" do
+    # Create an admin to receiving emails in #create
+    let(:admin){create(:user, :admin)}
+    before(:each){sign_in(admin)}
 
-  end
-  describe "create" do
+    let(:user){create(:user)}   #default user is an organization
+		before(:each){sign_in(user)}
 
-  end
-  describe "edit" do
+    let(:organization){user.organization}
 
-  end
-  describe "update" do
+    describe "GET #index" do
+      it "gets all organizations" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
 
-  end
-  describe "show" do
+    describe "GET #new" do
+      it "creates a new organization" do
+        get :new
+        expect(response).to be_successful
+      end
+    end
+    
+    describe "POST #create" do
+      it "saves new organization and saves it to current user" do
+        post :create, params: {organization: attributes_for(:organization)}
+        expect(response).to redirect_to(organization_application_submitted_path)
+      end
 
-  end
-  describe "approve" do
+      it "it did not save, create new organization" do
+        expect_any_instance_of(Organization).to receive(:save).and_return(false)
+        post :create, params: {organization: attributes_for(:organization)}
+        expect(response).to be_successful
+      end
+      
+    end
 
-  end
-  describe "reject" do
+    describe "PATCH #update" do
+      it "redirects to dashboard_path" do
+        post :update, params: { id: "", organization: {} }
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
 
+    describe "POST #approve" do
+      it "redirects to dashboard" do
+        post :approve, params: { id: "" }
+        expect(response).to redirect_to(dashboard_path)
+      end
+
+    end
+
+    describe "POST #reject" do
+      it "redirects to dashboard" do
+        post :reject, params: {id: ""}
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
   end
+
+  
+  context 'as an organization user without an Organization' do
+    
+    # Create an admin to receiving emails in #create
+    let(:admin){create(:user, :admin)}
+    before(:each){sign_in(admin)}
+
+    let(:user){create(:user)}   #default user is an organization
+		before(:each){sign_in(user)}
+
+    let(:organization){user.organization}
+
+    describe "GET #index" do
+      it "gets all organizations" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #new" do
+      it "creates a new organization" do
+        get :new
+        expect(response).to be_successful
+      end
+    end
+    
+    describe "POST #create" do
+      it "saves new organization and saves it to current user" do
+        post :create, params: {organization: attributes_for(:organization)}
+        expect(response).to redirect_to(organization_application_submitted_path)
+      end
+
+      it "it did not save, create new organization" do
+        expect_any_instance_of(Organization).to receive(:save).and_return(false)
+        post :create, params: {organization: attributes_for(:organization)}
+        expect(response).to be_successful
+      end
+      
+    end
+
+    describe "PATCH #update" do
+      it "redirects to dashboard_path" do
+        post :update, params: { id: "", organization: {} }
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    describe "POST #approve" do
+      it "redirects to dashboard" do
+        post :approve, params: { id: "" }
+        expect(response).to redirect_to(dashboard_path)
+      end
+
+    end
+
+    describe "POST #reject" do
+      it "redirects to dashboard" do
+        post :reject, params: {id: ""}
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+  end
+
 end
