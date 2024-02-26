bf4f4a08cd9a0d63ad74feb5da398aff78da2812
Devon Ceccacci <dceccacci@gmail.com>
2024-02-26 14:54:58 -0800

    super cool coding

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,13 +1,83 @@
 ﻿require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
-  describe "index" do
-
+  let(:admin){create(:user, :admin)}
+  let(:user){create(:user)}
+  let(:resCat){create(:resource_categories)}
+  
+  describe "GET #index" do
+    context "as an admin" do
+      let "get resource categories" do
+        before(:each){sign_in(admin)}
+        get :index
+        expect(response).to be_successful
+      end
+    end
+    context "as a user" do
+      let "get resource categories" do
+        before(:each){sign_in(user)}
+        get :index
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+    context "logged out" do
+      let "get resource categories" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
-  describe "show" do
 
+
+  describe "GET #show" do
+    context "as an admin" do
+      let "nothing" do
+        before(:each){sign_in(admin)}
+        get :show
+        expect(response).to be_empty
+      end
+    end
+    context "as a user" do
+      let "nothing" do
+        before(:each){sign_in(user)}
+        get :show
+        expect(response).to be_empty
+      end
+    end
+    context "logged out" do
+      let "nothing" do
+        get :show
+        expect(response).to be_empty
+      end
+    end
   end
-  describe "new" do
+
+
+
+  describe "GET #new" do
+    context "as an admin" do
+      let "be successful" do
+        before(:each){sign_in(admin)}
+        get :new
+        expect(response).not_to be_successful
+      end
+    end
+    context "as a user" do
+      let "redirect to dashboard" do
+        before(:each){sign_in(user)}
+        get :new
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+    context "logged out" do
+      let "redirect to new session" do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+
+
 
   end
   describe "create" do
--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+  before do
+    @region = create(:region)
+    @resource_category = create(:resource_category)
+  end
+
+  it 'can be created from the home screen' do
+    # User Input Part
+
+    visit root_path
+    click_on 'Get Help'
+    fill_in 'Full Name', with: 'Chris Example'
+    fill_in 'Phone Number', with: '555-555-1212'
+
+    #select "Bend", from: 'Region'
+    select @region.name, from: 'Region'
+    select @resource_category.name, from: 'Resource Category'
+
+    fill_in 'Description', with: 'Test description'
+
+    click_on 'Send this help request'
+
+    # Test Response part
+    expect(current_path).to eq ticket_submitted_path
+  end
 end
