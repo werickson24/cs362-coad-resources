2ce1768dcff7e3d4ea68fb41ebed8f1d3c0f5cc2
Devon Ceccacci <dceccacci@gmail.com>
2024-02-28 15:14:25 -0800

    added feature tests for regions and resource_categories

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  before do
+    @region = create(:region, name: "testRegion")
+    @user = create(:user, :admin)
+  end
+  it "can delete a region from the homescreen" do
+    log_in_as(@user)
+    visit root_path
+    click_on 'Regions'
+    click_on 'testRegion'
+    click_on 'Delete'
+    expect(page.body).to have_text('Region testRegion was deleted.')
+
+  end
 
 end
--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
 
+  before do
+    @region = create(:resource_category, name: "testCategory")
+    @user = create(:user, :admin)
+  end
+
+  it "can delete a resource category from the home screen" do
+    log_in_as(@user)
+    visit root_path
+    click_on "Categories"
+    click_on "testCategory"
+    click_on "Delete"
+    expect(page.body).to have_text('Category testCategory was deleted.')
+  end
 end
