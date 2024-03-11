c2f1d667ac5481fae3645ca12496f0be183c7e86
Devon Ceccacci <dceccacci@gmail.com>
2024-03-11 14:46:48 -0700

    attempted close_ticket

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
-  
+  before do
+      @user = create(:user, :organization_approved)
+      @ticket = create(:ticket, name: "testTicket")
+  end
+
+  it "can close a ticket from the home screen" do
+      log_in_as(@user)
+      visit root_path
+      click_on "Dashboard"
+      click_on "testTicket"
+      #puts(page.body)
+  end
+
+
 end
