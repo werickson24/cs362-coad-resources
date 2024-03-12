643975b908c0df8a703eeb8ac8425626a765147f
William Erickson <william@ericksonls.com>
2024-03-11 18:25:19 -0700

    Finished user registration end to end test

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+﻿require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+
+  it "user can sign up" do
+    email = generate(:email)
+    visit root_path
+    click_on('Sign up')
+    fill_in 'user_email', with: email
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button('commit')
+    expect(page.body).to have_text('confirmation link has been sent')
+  end
 
 end
