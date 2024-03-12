a4314a3db3537fbce74e2723a8ffaa28a0a5996e
William Erickson <william@ericksonls.com>
2024-03-11 18:15:58 -0700

    Finished user login end to end test

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+﻿require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+
+  it "user can log in" do
+    user = create(:user, :admin)
+    visit root_path
+    click_on('Log in')
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: 'password'
+    click_button('commit')
+    expect(page.body).to have_text('Signed in successfully.')
+  end
 
 end
